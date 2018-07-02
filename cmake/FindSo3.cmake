b3f75c8d3f29963ec6003ddef2efcf34c942bf83
luke <Luke.Pratley.15@ucl.ac.uk>
2018-07-02 01:10:58 +0100

    added missing find so3

--- cmake/FindSo3.cmake
+++ cmake/FindSo3.cmake
@@ -1,8 +1,8 @@
-find_path(So3_INCLUDE_DIR NAMES ssht.h PATH_SUFFIXES ssht)
-find_library(So3_LIBRARY NAMES ssht)
+find_path(So3_INCLUDE_DIR NAMES so3.h PATH_SUFFIXES so3)
+find_library(So3_LIBRARY NAMES so3)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
   So3  DEFAULT_MSG So3_INCLUDE_DIR So3_LIBRARY)
-MESSAGE(STATUS "Using ssht include ${So3_INCLUDE_DIR}")
-MESSAGE(STATUS "Using ssht lib ${So3_LIBRARY}")
+MESSAGE(STATUS "Using so3 include ${So3_INCLUDE_DIR}")
+MESSAGE(STATUS "Using so3 lib ${So3_LIBRARY}")
