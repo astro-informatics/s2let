c0732a70e5bb225eced0dbd88d3ab761fe84b9f3
Cosmomatt <m.price.17@ucl.ac.uk>
2018-07-18 10:54:27 +0100

    Added headers for adjoints

---
+++ include/s2let_axisym_denoising_demo.h
@@ -0,0 +1,25 @@
+// S2LET package
+// Copyright (C) 2012
+// Boris Leistedt & Jason McEwen
+
+#ifndef S2LET_AXISYM_DENOISE_DEMO
+#define S2LET_AXISYM_DENOISE_DEMO
+#include "ssht_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void s2let_lm_random_flm_real_sigma(complex double *flm, int L, int seed, double sigmanoise);
+
+//!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+double needletpower(double *wav_lm, int L);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ include/s2let_denoising_demo.h
@@ -0,0 +1,28 @@
+// S2LET package
+// Copyright (C) 2012
+// Boris Leistedt & Jason McEwen
+
+#ifndef S2LET_DENOISE_DEMO
+#define S2LET_DENOISE_DEMO
+#include "ssht_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void s2let_lm_random_flm_real_sigma(complex double *flm, int L, int seed, double sigmanoise);
+
+//!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+double waveletpower(complex double *wav_lm, int L);
+
+//!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void hard_threshold_real(double *g_wav, const double *threshold, const s2let_parameters_t *parameters); 
+
+#ifdef __cplusplus
+}
+#endif
+#endif
