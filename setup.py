09090cef13f270e633c85fd389eb89eed4105596
paddyroddy <patrickjamesroddy@gmail.com>
2020-07-06 13:52:46 +0100

    make pys2let compatible with Cython > 0.27.3

--- setup.py
+++ setup.py
@@ -1,48 +1,45 @@
-
-import sys
 import os
 import shutil
-
-from distutils.core import setup, Extension
-from Cython.Distutils import build_ext
-from Cython.Build import cythonize
+from distutils.core import Extension, setup
 
 import numpy
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
 
 # clean previous build
-for root, dirs, files in os.walk('./src/main/python/', topdown=False):
+for root, dirs, files in os.walk("./src/main/python/", topdown=False):
     for name in dirs:
-        if (name == 'build'):
+        if name == "build":
             shutil.rmtree(name)
 
 include_dirs = [
     numpy.get_include(),
-    './include',
-    os.environ['SSHT'] + '/src/c',
-    os.environ['SO3'] + '/src/c'
+    "./include",
+    os.environ["SSHT"] + "/src/c",
+    os.environ["SO3"] + "/src/c",
 ]
 
 extra_link_args = [
-    '-L./build',
-    '-L' + os.environ['FFTW'] + '/lib',
-    # depending whether used cmake
-    '-L' + os.environ['SSHT'] + '/build',
-    '-L' + os.environ['SO3'] + '/build',
-    # or make
-    '-L' + os.environ['SSHT'] + '/lib/c',
-    '-L' + os.environ['SO3'] + '/lib/c'
+    "-L./build",
+    "-L" + os.environ["FFTW"] + "/lib",
+    "-L" + os.environ["SSHT"] + "/build/src/c",
+    "-L" + os.environ["SO3"] + "/build",
+]
+
+extensions = [
+    Extension(
+        "pys2let",
+        sources=["src/main/python/pys2let.pyx"],
+        include_dirs=include_dirs,
+        libraries=["s2let", "so3", "ssht", "fftw3"],
+        extra_link_args=extra_link_args,
+        extra_compile_args=[],
+    )
 ]
 
 setup(
-    name='pys2let',
-    version='2.0',
-    cmdclass={'build_ext': build_ext},
-    ext_modules=cythonize([Extension(
-        'src/main/python/pys2let',
-        sources=['src/main/python/pys2let.pyx'],
-        include_dirs=include_dirs,
-        libraries=['s2let', 'so3', 'ssht', 'fftw3'],
-        extra_link_args=extra_link_args,
-        extra_compile_args=[]
-    )])
+    name="pys2let",
+    version="2.0",
+    cmdclass={"build_ext": build_ext},
+    ext_modules=cythonize(extensions),
 )
