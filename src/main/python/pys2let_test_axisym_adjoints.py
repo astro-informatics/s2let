bc4fa295bcfaedcc03ff9c1a8fbfde9089fdc2f5
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-07-08 22:10:46 +0100

    started axisym tests again

--- src/main/python/pys2let_test_axisym_adjoints.py
+++ src/main/python/pys2let_test_axisym_adjoints.py
@@ -1,128 +1,45 @@
 from pys2let import *
 import numpy as np
 
-# DOT PRODUCT TESTS
-# if y = Ax and g = A'f, show that f'y = g'x
+def random_lms(L):
+    return np.random.rand(L * L).astype(np.complex)  # not right for N > 1
+
+def random_mw_map(L):
+    return alm2map_mw(random_lms(L), L, 0)
+
+def random_wavlet_maps(L, nwvlts):
+    f_scal = random_mw_map(L)
+    f_wav = np.concatenate([random_mw_map(L) for _ in range(nwvlts)])
+    return f_scal, f_wav
+
 L = 10
 B = 2
 J_min = 2
-J = pys2let_j_max(B, L, J_min)
-nwvlts = J - J_min + 1
+nwvlts = pys2let_j_max(B, L, J_min) - J_min + 1
 
-def random_lms(L):
-    lms = np.zeros(L * L, dtype=np.complex)
-    for el in range(L):
-        em = 0
-        while em <= el:
-            rand = np.asarray(np.random.rand(), dtype=np.complex)
-            lms[el * el + el - em] = pow(-1.0, -em) * rand.conjugate()
-            lms[el * el + el + em] = rand
-            em += 1
-    return lms
-
-def random_mw_map(L):
-    f_lm = random_lms(L)
-    f = alm2map_mw(f_lm, L, 0)
-    return f
-
-def random_mw_wavelet_maps(L):
-    f_wav_mw = np.column_stack([random_mw_map(L) for _ in range(nwvlts)])
-    f_wav_mw = np.ascontiguousarray(f_wav_mw.flatten("F"))
-    f_scal_mw = np.ascontiguousarray(random_mw_map(L))
-    return f_scal_mw, f_wav_mw
-
-
-#  ANALYSIS
-# fwd: input = map, output = wavelet/scaling coeffs
-# some random input map
-x = random_mw_map(L)  # MW map
-
-# perform fwd transform via hp
-x_lm = map2alm_mw(x, L, 0)
-x_hp_lm = lm2lm_hp(x_lm, L)
-y_wav_hp_lm, y_scal_hp_lm = analysis_axisym_lm_wav(x_hp_lm, B, L, J_min)
-y_scal_mw = alm2map_mw(lm_hp2lm(y_scal_hp_lm, L), L, 0)
-y_wav_mw = np.zeros((mw_size(L) * nwvlts), dtype=np.complex)
-offset = mw_size(L)
-for i in range(nwvlts):
-    y_wav_mw[i*offset:(i+1)*offset] = alm2map_mw(
-        lm_hp2lm(np.ascontiguousarray(y_wav_hp_lm[:,i]), L), 
-        L, 0)
-y = np.concatenate((y_scal_mw, y_wav_mw))  # MW 
-
-# adj: input = wavelet/scaling coeffs, output = map
-# some random wavelet coeffs
-f_scal_mw, f_wav_mw = random_mw_wavelet_maps(L)
-f = np.concatenate((f_scal_mw, f_wav_mw))  # MW 
-
-# perform adjoint transform
-g = analysis_adjoint_axisym_wav_mw(f_wav_mw, f_scal_mw, B, L, J_min)  # MW map
-
-LHS = f.T.dot(y)
-RHS = g.T.dot(x)
-print(f"Analysis: {LHS}, {RHS}")
-print(f"Dot product test error: {LHS - RHS}")
-
-
-# SYNTHESIS
-# inv: input = wavelet/scaling coeffs, output = map 
-# some random wavelet coeffs
-x_scal_mw, x_wav_mw = random_mw_wavelet_maps(L)
-x = np.concatenate((x_scal_mw, x_wav_mw))
-
-# perform inverse transform via hp
-x_scal_hp_lm = lm2lm_hp(map2alm_mw(x_scal_mw, L, 0), L)
-x_wav_hp_lm = np.zeros((L*(L+1)//2, nwvlts), dtype=np.complex)
-offset = mw_size(L)
-for i in range(nwvlts):
-    lm = map2alm_mw(x_wav_mw[i*offset:(i+1)*offset], L, 0)
-    x_wav_hp_lm[:,i] = lm2lm_hp(lm, L)
-y_hp_lm = synthesis_axisym_lm_wav(x_wav_hp_lm, x_scal_hp_lm, B, L, J_min)
-y = alm2map_mw(lm_hp2lm(y_hp_lm, L), L, 0).real
-
-# adj: input = map, output = wavelet/scaling coeffs
-# some random map
+#### as in s2let_test.c
 f = random_mw_map(L)
+f_rec = random_mw_map(L)
 
-# perform inverse adjoint transform
-g_wav_mw, g_scal_mw = synthesis_adjoint_axisym_wav_mw(f, B, L, J_min)
-g = np.concatenate((g_scal_mw, g_wav_mw))
+f_wav, f_scal = analysis_axisym_wav_mw(f, B, L, J_min)
+f_wav_rec, f_scal_rec = analysis_axisym_wav_mw(f_rec, B, L, J_min)
 
-LHS = f.T.dot(y)
-RHS = g.T.dot(x)
-print(f"Synthesis: {LHS}, {RHS}")
-print(f"Dot product test error: {LHS - RHS}")
+f_rec = analysis_adjoint_axisym_wav_mw(f_wav, f_scal, B, L, J_min)
 
+dot_product_error = f_wav_rec.conj().dot(f_wav)
+dot_product_error += f_scal_rec.conj().dot(f_scal)
+dot_product_error -= f_rec.conj().dot(f)
 
-##########
-f1_lm = random_lms(L)
-f2_lm = random_lms(L)
+print(f"Dot product error: {dot_product_error}")
 
-f1_lm_hp = lm2lm_hp(f1_lm, L)
-f2_lm_hp = lm2lm_hp(f2_lm, L)
+#### as on website http://sepwww.stanford.edu/sep/prof/pvi/conj/paper_html/node9.html
+x = random_mw_map(L)
+y_scal, y_wav = random_wavlet_maps(L, nwvlts)
 
-f1_wav_lm_hp, f1_scal_lm_hp = analysis_axisym_lm_wav(f1_lm_hp, B, L, J_min)
-f2_wav_lm_hp, f2_scal_lm_hp = analysis_axisym_lm_wav(f2_lm_hp, B, L, J_min)
+y = analysis_adjoint_axisym_wav_mw(y_wav, y_scal, B, L, J_min)
+x_wav, x_scal = analysis_axisym_wav_mw(x, B, L, J_min)
 
-f1_scal = alm2map_mw(lm_hp2lm(f1_scal_lm_hp, L), L, 0)
-f2_scal = alm2map_mw(lm_hp2lm(f2_scal_lm_hp, L), L, 0)
-f1_wav = np.zeros(mw_size(L) * nwvlts, dtype=np.complex)
-f2_wav = np.zeros(mw_size(L) * nwvlts, dtype=np.complex)
-offset = mw_size(L)
-for j in range(nwvlts):
-    f1_wav[j*offset:(j+1)*offset] = alm2map_mw(
-        lm_hp2lm(
-            np.ascontiguousarray(f1_wav_lm_hp[:, j]), L
-        ), L, 0
-    )
-    f2_wav[j*offset:(j+1)*offset] = alm2map_mw(
-        lm_hp2lm(
-            np.ascontiguousarray(f2_wav_lm_hp[:, j]), L
-        ), L, 0
-    )
-
-f1 = alm2map_mw(f1_lm, L, 0)
-f2_rec_adj = analysis_adjoint_axisym_wav_mw(f2_wav, f1_wav, B, L, J_min)
-
-dot_error = f2_wav.dot(f1_wav) + f2_scal.dot(f1_scal) - f2_rec_adj.dot(f1)
-print(f"Dot error: {dot_error}")
+dot_product_error = y_wav.conj().dot(x_wav)
+dot_product_error += y_scal.conj().dot(x_scal)
+dot_product_error -= y.conj().dot(x)
+print(f"Dot product error: {dot_product_error}")
