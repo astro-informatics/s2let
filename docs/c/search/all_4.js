e3e34f57d3e892b72ec14880c0443162d92b8650
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2020-09-18 11:29:54 +0100

    Update documentation

--- docs/c/search/all_4.js
+++ docs/c/search/all_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['get_5fmax_5ferror',['get_max_error',['../s2let__test__csv_8c.html#a4d00c945ef5468ad68184f0bc1ada91a',1,'s2let_test_csv.c']]]
+  ['get_5fmax_5ferror_7',['get_max_error',['../s2let__test__csv_8c.html#a4d00c945ef5468ad68184f0bc1ada91a',1,'s2let_test_csv.c']]]
 ];
--- docs/c/search/classes_0.js
+++ docs/c/search/classes_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['s2let_5fparameters_5ft',['s2let_parameters_t',['../structs2let__parameters__t.html',1,'']]]
+  ['s2let_5fparameters_5ft_312',['s2let_parameters_t',['../structs2let__parameters__t.html',1,'']]]
 ];
