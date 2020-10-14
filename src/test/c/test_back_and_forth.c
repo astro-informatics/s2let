9c7b7b619a14a72763585425a03a7c495c2880cc
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2020-10-14 23:02:01 +0100

    Simplified and removed some tests

--- src/test/c/test_back_and_forth.c
+++ src/test/c/test_back_and_forth.c
@@ -9,36 +9,36 @@
 const int seed = 1;
 
 void test_transform_axisym_lm_wav(void **state) {
-  const s2let_parameters_t *const parameters = (const s2let_parameters_t *)*state;
+  const s2let_parameters_t parameters = *(s2let_parameters_t *)*state;
 
   double *wav_lm, *scal_lm;
 
   // Allocate the wavelet kernels
-  s2let_transform_axisym_lm_allocate_wav(&wav_lm, &scal_lm, parameters);
+  s2let_transform_axisym_lm_allocate_wav(&wav_lm, &scal_lm, &parameters);
 
   // Compute the wavelet kernels
-  s2let_transform_axisym_lm_wav(wav_lm, scal_lm, parameters);
+  s2let_transform_axisym_lm_wav(wav_lm, scal_lm, &parameters);
 
   complex double *f_wav_lm, *f_scal_lm, *flm, *flm_rec;
-  s2let_allocate_lm(&flm, parameters->L);
-  s2let_allocate_lm(&flm_rec, parameters->L);
+  s2let_allocate_lm(&flm, parameters.L);
+  s2let_allocate_lm(&flm_rec, parameters.L);
 
   // Generate a random spherical harmonic decomposition
-  s2let_lm_random_flm(flm, parameters->L, 0, seed);
+  s2let_lm_random_flm(flm, parameters.L, 0, seed);
 
   // Allocate space for the wavelet scales (their harmonic coefficients)
-  s2let_transform_axisym_lm_allocate_f_wav(&f_wav_lm, &f_scal_lm, parameters);
+  s2let_transform_axisym_lm_allocate_f_wav(&f_wav_lm, &f_scal_lm, &parameters);
 
   // Perform the wavelet transform through exact harmonic tiling
   s2let_transform_axisym_lm_wav_analysis(
-      f_wav_lm, f_scal_lm, flm, wav_lm, scal_lm, parameters);
+      f_wav_lm, f_scal_lm, flm, wav_lm, scal_lm, &parameters);
 
   // Reconstruct the initial harmonic coefficients from those of the wavelets
   s2let_transform_axisym_lm_wav_synthesis(
-      flm_rec, f_wav_lm, f_scal_lm, wav_lm, scal_lm, parameters);
+      flm_rec, f_wav_lm, f_scal_lm, wav_lm, scal_lm, &parameters);
 
   // Compute the maximum absolute error on the harmonic coefficients
-  for (int i = 0; i < parameters->L * parameters->L; i += 1) {
+  for (int i = 0; i < parameters.L * parameters.L; i += 1) {
     assert_float_equal(creal(flm[i]), creal(flm_rec[i]), 1e-12);
     assert_float_equal(cimag(flm[i]), cimag(flm_rec[i]), 1e-12);
   }
@@ -52,36 +52,36 @@ void test_transform_axisym_lm_wav(void **state) {
 }
 
 void test_transform_axisym_lm_wav_multires(void **state) {
-  const s2let_parameters_t *const parameters = (const s2let_parameters_t *)*state;
+  const s2let_parameters_t parameters = *(s2let_parameters_t *)*state;
 
   double *wav_lm, *scal_lm;
 
   // Allocate the wavelet kernels
-  s2let_transform_axisym_lm_allocate_wav(&wav_lm, &scal_lm, parameters);
+  s2let_transform_axisym_lm_allocate_wav(&wav_lm, &scal_lm, &parameters);
 
   // Compute the wavelet kernels
-  s2let_transform_axisym_lm_wav(wav_lm, scal_lm, parameters);
+  s2let_transform_axisym_lm_wav(wav_lm, scal_lm, &parameters);
 
   complex double *f_wav_lm, *f_scal_lm, *flm, *flm_rec;
-  s2let_allocate_lm(&flm, parameters->L);
-  s2let_allocate_lm(&flm_rec, parameters->L);
+  s2let_allocate_lm(&flm, parameters.L);
+  s2let_allocate_lm(&flm_rec, parameters.L);
 
   // Generate a random spherical harmonic decomposition
-  s2let_lm_random_flm(flm, parameters->L, 0, seed);
+  s2let_lm_random_flm(flm, parameters.L, 0, seed);
 
   // Allocate space for the wavelet scales (their harmonic coefficients)
-  s2let_transform_axisym_lm_allocate_f_wav_multires(&f_wav_lm, &f_scal_lm, parameters);
+  s2let_transform_axisym_lm_allocate_f_wav_multires(&f_wav_lm, &f_scal_lm, &parameters);
 
   // Perform the wavelet transform through exact harmonic tiling
   s2let_transform_axisym_lm_wav_analysis_multires(
-      f_wav_lm, f_scal_lm, flm, wav_lm, scal_lm, parameters);
+      f_wav_lm, f_scal_lm, flm, wav_lm, scal_lm, &parameters);
 
   // Reconstruct the initial harmonic coefficients from those of the wavelets
   s2let_transform_axisym_lm_wav_synthesis_multires(
-      flm_rec, f_wav_lm, f_scal_lm, wav_lm, scal_lm, parameters);
+      flm_rec, f_wav_lm, f_scal_lm, wav_lm, scal_lm, &parameters);
 
   // Compute the maximum absolute error on the harmonic coefficients
-  for (int i = 0; i < parameters->L * parameters->L; i += 1) {
+  for (int i = 0; i < parameters.L * parameters.L; i += 1) {
     assert_float_equal(creal(flm[i]), creal(flm_rec[i]), 1e-12);
     assert_float_equal(cimag(flm[i]), cimag(flm_rec[i]), 1e-12);
   }
@@ -95,43 +95,43 @@ void test_transform_axisym_lm_wav_multires(void **state) {
 }
 
 void test_wav_transform_wavlm_manual(void **state) {
-  s2let_parameters_t *const parameters = (s2let_parameters_t *)*state;
+  const s2let_parameters_t parameters = *(s2let_parameters_t *)*state;
 
-  const int J = s2let_j_max(parameters) - parameters->J_min;
+  const int J = s2let_j_max(&parameters) - parameters.J_min;
 
   complex double *psi;
   double *phi;
-  s2let_tiling_wavelet_allocate(&psi, &phi, parameters);
-  s2let_tiling_wavelet(psi, phi, parameters);
+  s2let_tiling_wavelet_allocate(&psi, &phi, &parameters);
+  s2let_tiling_wavelet(psi, phi, &parameters);
 
   const int scal_bandlimit =
-      parameters->upsample
-          ? parameters->L
-          : MIN(s2let_bandlimit(parameters->J_min - 1, parameters), parameters->L);
+      parameters.upsample
+          ? parameters.L
+          : MIN(s2let_bandlimit(parameters.J_min - 1, &parameters), parameters.L);
 
   complex double *wav_l = (complex double *)calloc(
-      (J + 1) * parameters->L * parameters->L, sizeof(complex double));
+      (J + 1) * parameters.L * parameters.L, sizeof(complex double));
   int *wav_bandlimits = (int *)calloc(J + 1, sizeof(int));
   for (int j = 0; j <= J; ++j) {
     wav_bandlimits[j] =
-        parameters->upsample
-            ? parameters->L
-            : MIN(s2let_bandlimit(parameters->J_min + j, parameters), parameters->L);
-    for (int i = 0; i < parameters->L * parameters->L; ++i)
-      wav_l[j * parameters->L * parameters->L + i] =
-          psi[(parameters->J_min + j) * parameters->L * parameters->L + i];
+        parameters.upsample
+            ? parameters.L
+            : MIN(s2let_bandlimit(parameters.J_min + j, &parameters), parameters.L);
+    for (int i = 0; i < parameters.L * parameters.L; ++i)
+      wav_l[j * parameters.L * parameters.L + i] =
+          psi[(parameters.J_min + j) * parameters.L * parameters.L + i];
   }
 
   complex double *f_wav, *f_scal, *flm, *flm_rec;
-  s2let_allocate_lm(&flm, parameters->L);
-  s2let_allocate_lm(&flm_rec, parameters->L);
+  s2let_allocate_lm(&flm, parameters.L);
+  s2let_allocate_lm(&flm_rec, parameters.L);
 
   // Generate a random spherical harmonic decomposition
-  s2let_lm_random_flm(flm, parameters->L, parameters->spin, seed);
+  s2let_lm_random_flm(flm, parameters.L, parameters.spin, seed);
 
   // Allocate space for the wavelet scales (their harmonic/Wigner coefficients)
   s2let_allocate_f_wav_manual(
-      &f_wav, &f_scal, wav_bandlimits, scal_bandlimit, parameters->N, J, parameters);
+      &f_wav, &f_scal, wav_bandlimits, scal_bandlimit, parameters.N, J, &parameters);
 
   // Perform the wavelet transform through exact harmonic tiling
   s2let_analysis_lm2wav_manual(
@@ -143,9 +143,9 @@ void test_wav_transform_wavlm_manual(void **state) {
       scal_bandlimit,
       wav_bandlimits,
       J,
-      parameters->L,
-      parameters->spin,
-      parameters->N);
+      parameters.L,
+      parameters.spin,
+      parameters.N);
 
   // Reconstruct the initial harmonic coefficients from those of the wavelets
   s2let_synthesis_wav2lm_manual(
@@ -157,11 +157,11 @@ void test_wav_transform_wavlm_manual(void **state) {
       scal_bandlimit,
       wav_bandlimits,
       J,
-      parameters->L,
-      parameters->spin,
-      parameters->N);
+      parameters.L,
+      parameters.spin,
+      parameters.N);
 
-  for (int i = 0; i < parameters->L * parameters->L; i += 1) {
+  for (int i = 0; i < parameters.L * parameters.L; i += 1) {
     assert_float_equal(creal(flm[i]), creal(flm_rec[i]), 1e-12);
     assert_float_equal(cimag(flm[i]), cimag(flm_rec[i]), 1e-12);
   }
@@ -175,22 +175,22 @@ void test_wav_transform_wavlm_manual(void **state) {
 }
 
 void test_wav_transform_harmonic(void **state) {
-  s2let_parameters_t *const parameters = (s2let_parameters_t *)*state;
+  const s2let_parameters_t parameters = *(s2let_parameters_t *)*state;
   complex double *psi;
   double *phi;
-  s2let_tiling_wavelet_allocate(&psi, &phi, parameters);
-  s2let_tiling_wavelet(psi, phi, parameters);
+  s2let_tiling_wavelet_allocate(&psi, &phi, &parameters);
+  s2let_tiling_wavelet(psi, phi, &parameters);
 
   complex double *f_wav_lmn, *f_scal_lm, *flm, *flm_rec;
-  s2let_allocate_lm(&flm, parameters->L);
-  s2let_allocate_lm(&flm_rec, parameters->L);
-  s2let_lm_random_flm(flm, parameters->L, parameters->spin, seed);
+  s2let_allocate_lm(&flm, parameters.L);
+  s2let_allocate_lm(&flm_rec, parameters.L);
+  s2let_lm_random_flm(flm, parameters.L, parameters.spin, seed);
 
-  s2let_allocate_lmn_f_wav(&f_wav_lmn, &f_scal_lm, parameters);
-  s2let_analysis_lm2lmn(f_wav_lmn, f_scal_lm, flm, psi, phi, parameters);
-  s2let_synthesis_lmn2lm(flm_rec, f_wav_lmn, f_scal_lm, psi, phi, parameters);
+  s2let_allocate_lmn_f_wav(&f_wav_lmn, &f_scal_lm, &parameters);
+  s2let_analysis_lm2lmn(f_wav_lmn, f_scal_lm, flm, psi, phi, &parameters);
+  s2let_synthesis_lmn2lm(flm_rec, f_wav_lmn, f_scal_lm, psi, phi, &parameters);
 
-  for (int i = 0; i < parameters->L * parameters->L; i += 1) {
+  for (int i = 0; i < parameters.L * parameters.L; i += 1) {
     assert_float_equal(creal(flm[i]), creal(flm_rec[i]), 1e-12);
     assert_float_equal(cimag(flm[i]), cimag(flm_rec[i]), 1e-12);
   }
@@ -204,39 +204,37 @@ void test_wav_transform_harmonic(void **state) {
 }
 
 void test_transform_axisym_wav(void **state) {
-  s2let_parameters_t *const parameters = (s2let_parameters_t *)*state;
-  if (parameters->spin != 0)
-    skip();
+  const s2let_parameters_t parameters = *(s2let_parameters_t *)*state;
 
   complex double *f, *f_rec, *flm, *flm_rec;
-  s2let_allocate_lm(&flm, parameters->L);
-  s2let_allocate_lm(&flm_rec, parameters->L);
-  s2let_allocate_mw(&f, parameters->L);
-  s2let_allocate_mw(&f_rec, parameters->L);
-  s2let_lm_random_flm(flm, parameters->L, 0, seed);
+  s2let_allocate_lm(&flm, parameters.L);
+  s2let_allocate_lm(&flm_rec, parameters.L);
+  s2let_allocate_mw(&f, parameters.L);
+  s2let_allocate_mw(&f_rec, parameters.L);
+  s2let_lm_random_flm(flm, parameters.L, parameters.spin, seed);
 
   ssht_core_mw_inverse_sov_sym(
       f,
       flm,
-      parameters->L,
-      parameters->spin,
-      parameters->dl_method,
-      parameters->verbosity);
+      parameters.L,
+      parameters.spin,
+      parameters.dl_method,
+      parameters.verbosity);
 
   complex double *f_wav, *f_scal;
-  s2let_transform_axisym_allocate_mw_f_wav(&f_wav, &f_scal, parameters);
-  s2let_transform_axisym_wav_analysis_mw(f_wav, f_scal, f, parameters);
-  s2let_transform_axisym_wav_synthesis_mw(f_rec, f_wav, f_scal, parameters);
+  s2let_transform_axisym_allocate_mw_f_wav(&f_wav, &f_scal, &parameters);
+  s2let_transform_axisym_wav_analysis_mw(f_wav, f_scal, f, &parameters);
+  s2let_transform_axisym_wav_synthesis_mw(f_rec, f_wav, f_scal, &parameters);
 
   ssht_core_mw_forward_sov_conv_sym(
       flm_rec,
       f_rec,
-      parameters->L,
-      parameters->spin,
-      parameters->dl_method,
-      parameters->verbosity);
+      parameters.L,
+      parameters.spin,
+      parameters.dl_method,
+      parameters.verbosity);
 
-  for (int i = 0; i < parameters->L * parameters->L; i += 1) {
+  for (int i = 0; i < parameters.L * parameters.L; i += 1) {
     assert_float_equal(creal(flm[i]), creal(flm_rec[i]), 1e-12);
     assert_float_equal(cimag(flm[i]), cimag(flm_rec[i]), 1e-12);
   }
@@ -283,39 +281,39 @@ void test_transform_axisym_wav_real(void **state) {
 }
 
 void test_transform_axisym_wav_multires(void **state) {
-  s2let_parameters_t *const parameters = (s2let_parameters_t *)*state;
-  if (parameters->spin != 0)
+  const s2let_parameters_t parameters = *(s2let_parameters_t *)*state;
+  if (parameters.spin != 0)
     skip();
 
   complex double *f, *f_rec, *flm, *flm_rec;
-  s2let_allocate_lm(&flm, parameters->L);
-  s2let_allocate_lm(&flm_rec, parameters->L);
-  s2let_allocate_mw(&f, parameters->L);
-  s2let_allocate_mw(&f_rec, parameters->L);
-  s2let_lm_random_flm(flm, parameters->L, 0, seed);
+  s2let_allocate_lm(&flm, parameters.L);
+  s2let_allocate_lm(&flm_rec, parameters.L);
+  s2let_allocate_mw(&f, parameters.L);
+  s2let_allocate_mw(&f_rec, parameters.L);
+  s2let_lm_random_flm(flm, parameters.L, 0, seed);
 
   ssht_core_mw_inverse_sov_sym(
       f,
       flm,
-      parameters->L,
-      parameters->spin,
-      parameters->dl_method,
-      parameters->verbosity);
+      parameters.L,
+      parameters.spin,
+      parameters.dl_method,
+      parameters.verbosity);
 
   complex double *f_wav, *f_scal;
-  s2let_transform_axisym_allocate_mw_f_wav_multires(&f_wav, &f_scal, parameters);
-  s2let_transform_axisym_wav_analysis_mw_multires(f_wav, f_scal, f, parameters);
-  s2let_transform_axisym_wav_synthesis_mw_multires(f_rec, f_wav, f_scal, parameters);
+  s2let_transform_axisym_allocate_mw_f_wav_multires(&f_wav, &f_scal, &parameters);
+  s2let_transform_axisym_wav_analysis_mw_multires(f_wav, f_scal, f, &parameters);
+  s2let_transform_axisym_wav_synthesis_mw_multires(f_rec, f_wav, f_scal, &parameters);
 
   ssht_core_mw_forward_sov_conv_sym(
       flm_rec,
       f_rec,
-      parameters->L,
-      parameters->spin,
-      parameters->dl_method,
-      parameters->verbosity);
+      parameters.L,
+      parameters.spin,
+      parameters.dl_method,
+      parameters.verbosity);
 
-  for (int i = 0; i < parameters->L * parameters->L; i += 1) {
+  for (int i = 0; i < parameters.L * parameters.L; i += 1) {
     assert_float_equal(creal(flm[i]), creal(flm_rec[i]), 1e-12);
     assert_float_equal(cimag(flm[i]), cimag(flm_rec[i]), 1e-12);
   }
@@ -363,38 +361,38 @@ void test_transform_axisym_wav_multires_real(void **state) {
 }
 
 void test_wav_transform_mw(void **state) {
-  s2let_parameters_t *const parameters = (s2let_parameters_t *)*state;
+  const s2let_parameters_t parameters = *(s2let_parameters_t *)*state;
 
   complex double *f, *f_rec, *flm, *flm_rec;
-  s2let_allocate_lm(&flm, parameters->L);
-  s2let_allocate_lm(&flm_rec, parameters->L);
-  s2let_allocate_mw(&f, parameters->L);
-  s2let_allocate_mw(&f_rec, parameters->L);
+  s2let_allocate_lm(&flm, parameters.L);
+  s2let_allocate_lm(&flm_rec, parameters.L);
+  s2let_allocate_mw(&f, parameters.L);
+  s2let_allocate_mw(&f_rec, parameters.L);
 
-  s2let_lm_random_flm(flm, parameters->L, parameters->spin, seed);
+  s2let_lm_random_flm(flm, parameters.L, parameters.spin, seed);
 
   ssht_core_mw_inverse_sov_sym(
       f,
       flm,
-      parameters->L,
-      parameters->spin,
-      parameters->dl_method,
-      parameters->verbosity);
+      parameters.L,
+      parameters.spin,
+      parameters.dl_method,
+      parameters.verbosity);
 
   complex double *f_wav, *f_scal;
-  s2let_allocate_f_wav(&f_wav, &f_scal, parameters);
-  s2let_analysis_px2wav(f_wav, f_scal, f, parameters);
-  s2let_synthesis_wav2px(f_rec, f_wav, f_scal, parameters);
+  s2let_allocate_f_wav(&f_wav, &f_scal, &parameters);
+  s2let_analysis_px2wav(f_wav, f_scal, f, &parameters);
+  s2let_synthesis_wav2px(f_rec, f_wav, f_scal, &parameters);
 
   ssht_core_mw_forward_sov_conv_sym(
       flm_rec,
       f_rec,
-      parameters->L,
-      parameters->spin,
-      parameters->dl_method,
-      parameters->verbosity);
+      parameters.L,
+      parameters.spin,
+      parameters.dl_method,
+      parameters.verbosity);
 
-  for (int i = 0; i < parameters->L * parameters->L; i += 1) {
+  for (int i = 0; i < parameters.L * parameters.L; i += 1) {
     assert_float_equal(creal(flm[i]), creal(flm_rec[i]), 1e-12);
     assert_float_equal(cimag(flm[i]), cimag(flm_rec[i]), 1e-12);
   }
@@ -410,8 +408,6 @@ void test_wav_transform_mw(void **state) {
 void test_wav_transform_mw_real(void **state) {
   s2let_parameters_t parameters = *(s2let_parameters_t *)*state;
   parameters.reality = 1;
-  if (parameters.spin != 0)
-    skip();
 
   double *f, *f_rec;
   complex double *flm, *flm_rec;
@@ -496,8 +492,6 @@ void test_wav_transform_mwss(void **state) {
 void test_wav_transform_mwss_real(void **state) {
   s2let_parameters_t parameters = *(s2let_parameters_t *)*state;
   parameters.sampling_scheme = S2LET_SAMPLING_MW_SS;
-  if (parameters.spin != 0)
-    skip();
 
   double *f, *f_rec;
   complex double *flm, *flm_rec;
@@ -558,8 +552,6 @@ void test_wav_transform_lm2wav(void **state) {
 void test_wav_transform_lm2wav_real(void **state) {
   s2let_parameters_t parameters = *(s2let_parameters_t *)*state;
   parameters.reality = 1;
-  if (parameters.spin != 0)
-    skip();
 
   complex double *flm, *flm_rec;
   s2let_allocate_lm(&flm, parameters.L);
@@ -604,29 +596,31 @@ int main(void) {
   const s2let_parameters_t spintwo = state(2, 1);
   const s2let_parameters_t spintwo_noupsample = state(2, 0);
 
+  const struct CMUnitTest tests[] = {
 #define S2LET_TESTS(name)                                                              \
   cmocka_unit_test_prestate(name, (void *)&spinzero_noupsample),                       \
       cmocka_unit_test_prestate(name, (void *)&spinzero),                              \
       cmocka_unit_test_prestate(name, (void *)&spintwo),                               \
       cmocka_unit_test_prestate(name, (void *)&spintwo_noupsample)
-
-  const struct CMUnitTest tests[] = {
       S2LET_TESTS(test_transform_axisym_lm_wav),
       S2LET_TESTS(test_transform_axisym_lm_wav_multires),
       S2LET_TESTS(test_wav_transform_wavlm_manual),
       S2LET_TESTS(test_wav_transform_harmonic),
+      S2LET_TESTS(test_wav_transform_mw),
+      S2LET_TESTS(test_wav_transform_mwss),
+      S2LET_TESTS(test_wav_transform_lm2wav),
+#undef S2LET_TESTS
+
+#define S2LET_TESTS(name) cmocka_unit_test_prestate(name, (void *)&spinzero)
       S2LET_TESTS(test_transform_axisym_wav),
       S2LET_TESTS(test_transform_axisym_wav_multires),
       S2LET_TESTS(test_transform_axisym_wav_real),
       S2LET_TESTS(test_transform_axisym_wav_multires_real),
-      S2LET_TESTS(test_wav_transform_mw),
       S2LET_TESTS(test_wav_transform_mw_real),
-      S2LET_TESTS(test_wav_transform_mwss),
       S2LET_TESTS(test_wav_transform_mwss_real),
-      S2LET_TESTS(test_wav_transform_lm2wav),
       S2LET_TESTS(test_wav_transform_lm2wav_real),
-  };
 #undef S2LET_TESTS
+  };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
