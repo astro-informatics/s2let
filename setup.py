a0c379a081c4b0c250511b4939b842bcd9527a6b
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2021-09-29 09:18:11 +0100

    Bump to 2.2.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ build_requirements = [
 
 setup(
     name="pys2let",
-    version="2.2.3",
+    version="2.2.4",
     author=["Boris Leistedt", "Martin Büttner", "Jennifer Chan", "Jason McEwen"],
     install_requires=["numpy"],
     extras_require={
